a1a59507ed7935bbb6fdc8e9daaa5e789e238e06
Zuwayr <50750289+Zuwayr@users.noreply.github.com>
2020-10-28 16:36:27 +0500

    Update database.sh

--- database.sh
+++ database.sh
@@ -3,15 +3,3 @@ mkdir -p /var/run/postgresql/9.6-main.pg_stat_tmp
 chown postgres.postgres /var/run/postgresql/9.6-main.pg_stat_tmp -R
 chown -R postgres:postgres /var/run/postgresql
 su -c "nohup /usr/lib/postgresql/9.6/bin/postgres -D /var/lib/postgresql/9.6/main -c config_file=/etc/postgresql/9.6/main/postgresql.conf > /dev/null 2>&1 &" postgres
-sleep 10
-psql --command "CREATE TABLE students (
-	first_name VARCHAR ( 25 ) NOT NULL,
-	last_name VARCHAR ( 25 ) NOT NULL,
-	id INT UNIQUE NOT NULL
-);"
-psql --command "INSERT INTO 
-	students (first_name, last_name, id)
-	VALUES
-	('Leslie',	'Knope',	'43102'),
-	('Tom',	'Haverford',	'73210'),
-	('Ron',	'Swanson',	'24758');"
