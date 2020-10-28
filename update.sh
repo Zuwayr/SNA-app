13ea7efe6def0fef7dd92634a82747025c8b5f07
Zuwayr <50750289+Zuwayr@users.noreply.github.com>
2020-10-28 16:35:43 +0500

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
-psql --command "DROP TABLE students"
-psql --command "CREATE TABLE students (
+su -c 'psql --command "DROP TABLE students"' postgres
+su -c 'psql --command "CREATE TABLE students (
 	first_name VARCHAR ( 25 ) NOT NULL,
 	last_name VARCHAR ( 25 ) NOT NULL,
 	id INT UNIQUE NOT NULL
-);"
-psql --command "INSERT INTO 
-	students (first_name, last_name, id)
-	VALUES
-	('Leslie',	'Knope',	'43102'),
-	('Tom',	'Haverford',	'73210'),
-	('Ron',	'Swanson',	'24758');"
+);"' postgres
+su -c 'psql postgres < outfile' postgres
